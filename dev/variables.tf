a918ab22f0160f2a89cf4549067ab61e86d0c1ca
Santos Rodriguez <37907123+santosrodriguez@users.noreply.github.com>
2025-11-01 08:22:47 -0400

    Update variables.tf

--- dev/variables.tf
+++ dev/variables.tf
@@ -1,1 +1,21 @@
+variable "prefix" {
+  type        = string
+  description = "(Optional) The prefix which should be used for all resources in this example. Defaults to taco."
+  default     = "burrito"
+}
 
+variable "location" {
+  type        = string
+  description = "(Optional) The Azure Region in which all resources in this example should be created. Defaults to East US."
+  default     = "eastus"
+}
+
+variable "environment" {
+  type        = string
+  description = "(Required) The environment in which all resources in this example should be created."
+}
+
+variable "env_version" {
+  type        = string
+  description = "(Required) Update to force a change."
+}
