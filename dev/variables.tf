90481441d0f0193151a460f4fc588a62a8cb52df
Santos Rodriguez <37907123+santosrodriguez@users.noreply.github.com>
2025-11-01 16:39:34 -0400

    Update variables.tf

--- dev/variables.tf
+++ dev/variables.tf
@@ -1,7 +1,7 @@
 variable "prefix" {
   type        = string
   description = "(Optional) The prefix which should be used for all resources in this example. Defaults to taco."
-  default     = "burrito"
+  default     = "burger"
 }
 
 variable "location" {
