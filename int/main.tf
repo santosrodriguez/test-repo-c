4ac0c90fc2fc0636a932f006fe900d1b45d94e12
Santos Rodriguez <37907123+santosrodriguez@users.noreply.github.com>
2025-11-01 08:01:37 -0400

    Update main.tf

--- int/main.tf
+++ int/main.tf
@@ -6,7 +6,7 @@ provider "azurerm" {
 variable "prefix" {
   type        = string
   description = "(Optional) The prefix which should be used for all resources in this example. Defaults to taco."
-  default     = "taco"
+  default     = "burrito"
 }
 
 variable "location" {
